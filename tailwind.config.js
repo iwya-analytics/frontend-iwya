d0a60edc564246c58e7ea819b400db1064a033b6
Unknown <achrafatauil@hotmail.com>
2022-07-03 17:48:13 +0200

    setup tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+    fontFamily: {
+      'serif': ['Playfair Display', 'serif', 'ui-serif', 'Georgia'],
+    }
+  },
+  plugins: [],
+}
