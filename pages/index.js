0fc7a758c9a6b639739c99eb3b25f5e7885b9433
Achraf Atauil <achrafatauil@hotmail.com>
2022-07-13 17:22:28 +0200

    update header text

--- pages/index.js
+++ pages/index.js
@@ -28,7 +28,7 @@ export default function Home() {
           <div className='bg-zinc-900 flex flex-col md:flex-row text-white px-12 md:px-24 py-24 md:py-36'>
             <div className='flex-1'>
               <h1 className='text-3xl md:text-5xl mb-5 font-serif font-medium'>We help you build your online presence</h1>
-              <p className='text-gray-100 mb-12 md:mb-24 font-light'>In an online era, your online presence is almost inevatably linked with your companies success. We can help you both build or expand your online presence.</p>
+              <p className='text-gray-100 mb-12 md:mb-24 font-light'>In this online era, your online presence is almost inevatably linked with your companies success. At IWYA we understand this, we have been helping our clients get the most out of their digital platforms for several years now. We can help you both build and expand your online presence.</p>
               <a href="https://koalendar.com/events/Meet-with-Mohamed-Abouchama" className='px-12 py-4 bg-white text-black font-serif font-medium'>Let’s meet</a>
             </div>
             <div className='flex-1'>
